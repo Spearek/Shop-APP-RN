d74ec5ae94b61518c6ba4bcc3ed73fe3a23622f2
Spearek <30389214+Spearek@users.noreply.github.com>
2020-04-15 22:08:05 +0200

    navigation update

--- navigation/shopNavigator.js
+++ navigation/shopNavigator.js
@@ -3,18 +3,28 @@ import { createStackNavigator } from 'react-navigation-stack';
 import { Platform } from 'react-native';
 
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
+import ProductDetailScreen from '../screens/shop/ProductsDetailScreen';
+import CartScreen from '../screens/shop/CartScreen';
 import Colors from '../constants/Colors';
 
 const ProductsNavigator = createStackNavigator(
     {
-        ProductsOverview: ProductsOverviewScreen
+        ProductsOverview: ProductsOverviewScreen,
+        ProductDetail: ProductDetailScreen,
+        Cart: CartScreen
     },
     {
         defaultNavigationOptions: {
             headerStyle: {
                 backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
             },
-            headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
+            headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
+            headerTitleStyle: {
+                fontFamily: 'open-sans-bold'
+            },
+            headerBackTitleStyle: {
+                fontFamily: 'open-sans'
+            }
         }
     }
 );
