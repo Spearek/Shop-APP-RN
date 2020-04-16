01ad2ae1c23779d51431e1b13cfa0af162a28bd2
Spearek <30389214+Spearek@users.noreply.github.com>
2020-04-16 14:43:25 +0200

    include dispatch for removing item from the cart

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { View, Text, FlatList, Button, StyleSheet } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Colors from '../../constants/Colors';
 import CartItem from '../../components/shop/CartItem';
+
+import * as cartActions from '../../store/actions/cart'
 
 const CartScreen = props => {
     const cartTotalAmmount = useSelector(state => state.cart.totalAmmount);
@@ -18,8 +20,10 @@ const CartScreen = props => {
                 sum: state.cart.items[key].sum
             })
         }
-        return transformedCartItems
+        return transformedCartItems.sort((a, b) => a.productId >= b.productId ? 1 : -1)
     });
+
+    const dispatch = useDispatch();
 
     return (
         <View style={styles.screen}>
@@ -29,10 +33,18 @@ const CartScreen = props => {
                 </Text>
                 <Button color={Colors.accent} title="Order Now" disabled={cartItems.length === 0} />
             </View>
-            <FlatList 
-            data={cartItems}
-            keyExtractor={item =>item.productId}
-            renderItem={itemData => <CartItem quantity = {itemData.item.quantity} title={itemData.item.productTitle} amount={itemData.item.sum} onRemove={() =>{}} />}
+            <FlatList
+                data={cartItems}
+                keyExtractor={item => item.productId}
+                renderItem={itemData => (
+                    <CartItem
+                        quantity={itemData.item.quantity}
+                        title={itemData.item.productTitle}
+                        amount={itemData.item.sum}
+                        onRemove={() => {
+                            dispatch(cartActions.removeFromCart(itemData.item.productId));
+                        }} />
+                )}
             />
         </View>
     )
