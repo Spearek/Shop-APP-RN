bafa0a3d20bdbba365ed67a3d50bd0a0038f05bc
Spearek <30389214+Spearek@users.noreply.github.com>
2020-04-26 11:47:40 +0200

    admin navigator created

--- navigation/shopNavigator.js
+++ navigation/shopNavigator.js
@@ -8,6 +8,7 @@ import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
 import ProductDetailScreen from '../screens/shop/ProductsDetailScreen';
 import CartScreen from '../screens/shop/CartScreen';
 import OrdersScreen from '../screens/shop/OrdersScreen';
+import UserProductsScreen from '../screens/user/UserProductsScreen';
 import Colors from '../constants/Colors';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -57,10 +58,32 @@ const OrdersNavigator = createStackNavigator({
         }
     },
 )
+const AdminNavigator = createStackNavigator({
+    UserProducts: UserProductsScreen
+},
+    {
+        navigationOptions: {
+            drawerIcon: drawerConfig => <Ionicons name={'md-create'} size={23} color={drawerConfig.tintColor} />
+        },
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
+            },
+            headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
+            headerTitleStyle: {
+                fontFamily: 'open-sans-bold'
+            },
+            headerBackTitleStyle: {
+                fontFamily: 'open-sans'
+            }
+        }
+    },
+)
 
 const shopNavigator = createDrawerNavigator({
     Products: ProductsNavigator,
-    Orders: OrdersNavigator
+    Orders: OrdersNavigator,
+    Admin: AdminNavigator
 }, {
     contentOptions: {
         activeTintColor: Colors.primary
