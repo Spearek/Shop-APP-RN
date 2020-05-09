24979965388958180871f75a7ea3c6fbe4738c42
Spearek <30389214+Spearek@users.noreply.github.com>
2020-05-09 13:02:45 +0200

    action creators and reducer prepared for creating new products and editing existing ones

--- store/reducers/products.js
+++ store/reducers/products.js
@@ -1,6 +1,7 @@
 
 import PRODUCTS from '../../data/dummy-data';
-import { DELETE_PRODUCT } from '../actions/products';
+import { DELETE_PRODUCT, CREATE_PRODUCT, UPDATE_PRODUCT } from '../actions/products';
+import Product from '../../models/product';
 
 
 const initialState = {
@@ -8,14 +9,50 @@ const initialState = {
     userProducts: PRODUCTS.filter(prod => prod.ownerId === 'u1')
 }
 
-export default (state = initialState, action ) => {
+export default (state = initialState, action) => {
     switch (action.type) {
-        case DELETE_PRODUCT: 
-        return {
-            ...state,
-            userProducts: state.userProducts.filter(product => product.id !== action.pid), // ponieważ filter z automatu zwraca NOWĄ tablicę
-            avaliableProducts: state.avaliableProducts.filter(product => product.id !== action.pid)
-        }
+        case CREATE_PRODUCT:
+            const newProduct = new Product(
+                new Date().toString(),
+                'u1',
+                action.productData.title,
+                action.productData.imageUrl,
+                action.productData.description,
+                action.productData.price
+            );
+            return {
+                ...state,
+                avaliableProducts: state.avaliableProducts.concat(newProduct),
+                userProducts: state.userProducts.concat(newProduct)
+            }
+        case UPDATE_PRODUCT:
+            const productIndex = state.userProducts.findIndex(prod => prod.id === action.pid);
+            const updatedProduct = new Product(
+                action.pid,
+                state.userProducts[productIndex].ownerId,
+                action.productData.title,
+                action.productData.imageUrl,
+                action.productData.description,
+                state.userProducts[productIndex].price
+            );
+            const updatedUserProducts = [...state.userProducts];
+            updatedUserProducts[productIndex] = updatedProduct;
+            const avaliableProductIndex = state.avaliableProducts.findIndex(prod => prod.id === action.pid);
+            const updatedAvaliableProducts = [...state.avaliableProducts];
+            updatedAvaliableProducts[avaliableProductIndex] = updatedProduct;
+
+            return {
+                ...state,
+                avaliableProducts: updatedAvaliableProducts,
+                userProducts: updatedUserProducts
+            }
+
+        case DELETE_PRODUCT:
+            return {
+                ...state,
+                userProducts: state.userProducts.filter(product => product.id !== action.pid), // ponieważ filter z automatu zwraca NOWĄ tablicę
+                avaliableProducts: state.avaliableProducts.filter(product => product.id !== action.pid)
+            }
     }
     return state;
 };
